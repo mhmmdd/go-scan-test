640b7a8c28cdc10885011bc165f77217967ed80c
mhmmdd <mhmmd.dd@gmail.com>
2021-12-31 14:43:55 +0300

    race detection

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var a string
-	fmt.Scanln(&a)
-	fmt.Println("test : " + a)
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a" // First conflicting access.
+		c <- true
+	}()
+	m["2"] = "b" // Second conflicting access.
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
